a92f4b727eecd559ec875064bcf805f02e6711b6
Ivan Santos <me@ivan-santos.dev>
2023-10-19 16:10:30 -0600

    Add new index

--- go.mod
+++ go.mod
@@ -32,7 +32,10 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/labstack/echo/v4 v4.11.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -40,12 +43,14 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,182 +1,45 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	log "github.com/sirupsen/logrus"
-	"os"
-	"strings"
+	"github.com/labstack/echo/v4"
+	"html/template"
+	"io"
+	"net/http"
 )
 
-func filterEmpty(slice []string) []string {
-	var result []string
-	for _, str := range slice {
-		if str != "" {
-			result = append(result, str)
-		}
-	}
-	return result
-}
-func GetFormerNameList(c *gin.Context) []string {
-	listStr, err := c.Cookie("former-names")
-	if err != nil {
-		log.Error("Failed to get cookie", err)
-	}
-	return filterEmpty(strings.Split(listStr, ","))
+type Template struct {
+	templates *template.Template
 }
 
-func AddFormerNameList(c *gin.Context, name string) []string {
-	list := GetFormerNameList(c)
-	list = addName(list, name)
-	SetFormerNameCookie(c, list)
-	return list
-}
-
-func RemoveFormerNameList(c *gin.Context, name string) []string {
-	list := GetFormerNameList(c)
-	list = removeName(list, name)
-	SetFormerNameCookie(c, list)
-	return list
-}
-
-func GetVipList(c *gin.Context) []string {
-	listStr, err := c.Cookie("vip-list")
-	if err != nil {
-		log.Error("Failed to get cookie", err)
-	}
-	return filterEmpty(strings.Split(listStr, ","))
-}
-
-func AddVipList(c *gin.Context, name string) []string {
-	list := GetVipList(c)
-	list = addName(list, name)
-	SetVipListCookie(c, list)
-	return list
-}
-
-func addName(list []string, name string) []string {
-	for _, n := range list {
-		if n == name {
-			return list
-		}
-	}
-	return append(list, name)
-}
-
-func RemoveVipList(c *gin.Context, name string) []string {
-	list := GetVipList(c)
-	list = removeName(list, name)
-	SetVipListCookie(c, list)
-	return list
-}
-
-func removeName(list []string, name string) []string {
-	for i, n := range list {
-		if n == name {
-			if len(list) == 1 {
-				list = []string{}
-				break
-			}
-			list = append(list[:i], list[i+1:]...)
-		}
-	}
-	return list
-}
-
-func SetVipListCookie(c *gin.Context, names []string) {
-	setCookie(c, "vip-list", names)
-}
-
-func SetFormerNameCookie(c *gin.Context, names []string) {
-	setCookie(c, "former-names", names)
-}
-
-func setCookie(c *gin.Context, cookieName string, names []string) {
-	if len(names) == 0 {
-		c.SetCookie(cookieName, "", -1, "/", os.Getenv("DOMAIN"), false, true)
-		return
-	}
-	c.SetCookie(cookieName, strings.Join(names, ","), 365*24*60*60, "/", os.Getenv("DOMAIN"), false, true)
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
 	}
 
-	db := DB{}
-	db.InitializeGorm()
+	e := echo.New()
+	e.Renderer = t
+	e.Static("/static", "static")
 
-	formerNameService := &FormerNameService{db: db.db}
-	vipListService := &VipListService{db: db.db}
-
-	go CheckWorlds(db.db)
-	go CheckFormerNames(db.db)
-
-	r := gin.Default()
-
-	r.LoadHTMLGlob("./templates/**/*")
-	r.Static("/static", "./static")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", "World!")
 	})
 
-	r.GET("/vip-list", func(c *gin.Context) {
-		c.HTML(200, "VipListTable.html", gin.H{
-			"VipList": db.GetVipList(GetVipList(c)),
-		})
+	e.POST("/search", func(c echo.Context) error {
+		name := c.FormValue("search_name")
+
+		return c.Render(http.StatusOK, "search-table", name)
 	})
 
-	r.PUT("/vip-list/:name", func(c *gin.Context) {
-		name := c.Params.ByName("name")
-		err := vipListService.CreateVipListFriend(name)
-		if err != nil {
-		}
+	e.PUT("/vip-list/:name", func(c echo.Context) error {
+		name := c.Param("name")
+		e.Logger.Info(name)
 
-		c.HTML(200, "VipListTable.html", gin.H{
-			"VipList": db.GetVipList(AddVipList(c, c.Params.ByName("name"))),
-		})
+		return c.Render(http.StatusOK, "index", nil)
 	})
 
-	r.POST("/search-vip-list", func(c *gin.Context) {
-		name := c.PostForm("name")
-		apiChar, err := GetCharacter(name)
-		if err != nil {
-			log.Error(err)
-		}
-
-		c.HTML(200, "SearchVipListModal.html", gin.H{"Char": apiChar.CharacterInfo, "SafeCharName": apiChar.CharacterInfo.Name})
-	})
-
-	r.DELETE("/vip-list/:name", func(c *gin.Context) {
-		c.HTML(200, "VipListTable.html", gin.H{
-			"VipList": db.GetVipList(RemoveVipList(c, c.Params.ByName("name"))),
-		})
-	})
-
-	r.GET("/former-names", func(c *gin.Context) {
-		c.HTML(200, "FormerNamesTable.html", gin.H{"FormerNames": db.GetFormerNames(GetFormerNameList(c))})
-	})
-
-	r.POST("/former-names", func(c *gin.Context) {
-		name := c.PostForm("name")
-		err := formerNameService.CreateFormerName(name)
-		var errorText string
-		if err != nil {
-			errorText = err.Error()
-		}
-
-		c.HTML(200, "FormerNamesTable.html", gin.H{"Error": errorText, "FormerNames": db.GetFormerNames(AddFormerNameList(c, name))})
-	})
-
-	r.DELETE("/former-names/:formerName", func(c *gin.Context) {
-		name := c.Params.ByName("formerName")
-		c.HTML(200, "FormerNamesTable.html", gin.H{
-			"FormerNames": db.GetFormerNames(RemoveFormerNameList(c, name)),
-		})
-	})
-
-	r.Run(os.Getenv("SERVER_URL"))
+	e.Logger.Fatal(e.Start("127.0.0.1:1323"))
 }
