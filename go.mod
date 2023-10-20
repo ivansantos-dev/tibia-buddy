6ce564f7d788e2d902df9765c6b55c4546d91aa7
Ivan Santos <me@ivan-santos.dev>
2023-10-20 15:49:12 -0600

    Add oauth authentication

--- go.mod
+++ go.mod
@@ -3,23 +3,35 @@ module tibiabuddy.com/main
 go 1.20
 
 require (
+	github.com/gorilla/sessions v1.2.1
+	github.com/labstack/echo-contrib v0.15.0
 	github.com/labstack/echo/v4 v4.11.2
 	github.com/labstack/gommon v0.4.0
+	github.com/markbates/goth v1.78.0
 	github.com/sirupsen/logrus v1.9.3
 	gorm.io/gorm v1.25.2
 )
 
 require (
+	cloud.google.com/go v0.67.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"github.com/gorilla/sessions"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
 	"html/template"
 	"io"
 	"net/http"
+	"os"
 )
 
 type Template struct {
@@ -46,12 +53,29 @@ type SearchData struct {
 func main() {
 	e := echo.New()
 
+	e.HideBanner = true
+	e.Logger.SetLevel(log.INFO)
+
+	store := sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
+	store.MaxAge(60 * 60 * 24 * 30)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true // HttpOnly should always be enabled
+	store.Options.Secure = false  // TODO set in env
+	gothic.Store = store
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Logger.SetLevel(log.INFO)
-	if l, ok := e.Logger.(*log.Logger); ok {
-		l.SetHeader("${time_rfc3339} ${level}")
-	}
+	e.Use(session.Middleware(store))
+
+	clientId := os.Getenv("GOOGLE_OAUTH_CLIENT_ID")
+	clientSecret := os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET")
+	clientCallback := os.Getenv("GOOGLE_OAUTH_CALLBACK_URL")
+	e.Logger.Info(clientId, clientSecret, clientCallback)
+	goth.UseProviders(google.New(
+		clientId,
+		clientSecret,
+		clientCallback,
+	))
 
 	t := &Template{
 		templates: template.Must(template.ParseGlob("templates/*.html")),
@@ -96,5 +120,49 @@ func main() {
 		return c.HTML(http.StatusOK, "<button class=\"secondary\" disabled>Added</button>")
 	})
 
-	e.Logger.Fatal(e.Start("127.0.0.1:1323"))
+	e.GET("/auth/:provider", func(c echo.Context) error {
+		providerName := c.Param("provider")
+		provider, err := goth.GetProvider(providerName)
+		req := c.Request()
+		res := c.Response()
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+		sess, err := provider.BeginAuth(gothic.SetState(req))
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+
+		authUrl, err := sess.GetAuthURL()
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+
+		err = gothic.StoreInSession(providerName, sess.Marshal(), req, res)
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+
+		return c.Redirect(http.StatusTemporaryRedirect, authUrl)
+	})
+	e.GET("/auth/:provider/callback", func(c echo.Context) error {
+		user, err := gothic.CompleteUserAuth(c.Response(), c.Request())
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+
+		e.Logger.Info("logged in as", user)
+		return c.Redirect(http.StatusFound, "/")
+	})
+
+	e.GET("/logout/:provider", func(c echo.Context) error {
+		err := gothic.Logout(c.Response(), c.Request())
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+
+		return c.Redirect(http.StatusFound, "/")
+	})
+
+	e.Logger.Fatal(e.Start("127.0.0.1:8090"))
 }
